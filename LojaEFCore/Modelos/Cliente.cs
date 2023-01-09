1d077c2605c2184e4e22b428f1a7724c751a8cd6
Caique Burssed <caique.burssed@gmail.com>
2023-01-09 17:31:14 -0300

    Adicionando Data Anotations

--- LojaEFCore/Modelos/Cliente.cs
+++ LojaEFCore/Modelos/Cliente.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace LojaEFCore.Domain
 {
+    [Table("Clientes")]
     class Cliente
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
+        [Column("Phone")]
         public string Telefone { get; set; }
+
+        [Required]
         public string CEP { get; set; }
+
+        [Required]
         public string Estado { get; set; }
+
+        [Required]
         public string Cidade { get; set; }
 
     }
