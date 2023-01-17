edc65c5b2fc33065137271945ac3721af6f226bd
Caique Burssed <caique.burssed@gmail.com>
2023-01-17 11:27:15 -0300

    Inserindo registros no banco

--- LojaEFCore/Data/ApplicationContext.cs
+++ LojaEFCore/Data/ApplicationContext.cs
@@ -10,6 +10,8 @@ namespace LojaEFCore.Data
     class ApplicationContext : DbContext
     {
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos{ get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
--- LojaEFCore/Program.cs
+++ LojaEFCore/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LojaEFCore.Domain;
+using LojaEFCore.Enums;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 
@@ -8,16 +10,37 @@ namespace LojaEFCore
     {
         static void Main(string[] args)
         {
-            using var db = new Data.ApplicationContext();
+            /*using var db = new Data.ApplicationContext();
 
             var existe = db.Database.GetPendingMigrations().Any();
 
             if (existe)
             {
                 //MessageBox.Show("Migrations pendentes...");
-            }
+            }*/
 
-            Console.WriteLine("Hello World!");
+            InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            //db.Produtos.Add(produto);
+            //db.Set<Produto>().Add(produto);
+            //db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total registros: {registros}");
         }
     }
 }
