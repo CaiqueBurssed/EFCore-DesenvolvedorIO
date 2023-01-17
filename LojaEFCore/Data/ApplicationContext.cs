3c4d94ca34c9b980083144f3a252e78155ffff8b
Caique Burssed <caique.burssed@gmail.com>
2023-01-17 17:19:56 -0300

    Configuracoes finais da ApplicationContext

--- LojaEFCore/Data/ApplicationContext.cs
+++ LojaEFCore/Data/ApplicationContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace LojaEFCore.Data
@@ -21,13 +22,38 @@ namespace LojaEFCore.Data
             optionsBuilder
                 .UseLoggerFactory(_logger)
                 .EnableSensitiveDataLogging()
-                .UseSqlServer(@"Server=localhost;Database=LojaEFCore;Trusted_Connection=True;");
+                .UseSqlServer(@"Server=localhost;Database=LojaEFCore;Trusted_Connection=True;",
+                    p => p.EnableRetryOnFailure
+                    (
+                        maxRetryCount: 2, 
+                        maxRetryDelay: TimeSpan.FromSeconds(5), 
+                        errorNumbersToAdd: null)
+                    . MigrationsHistoryTable("curso_ef_core", null)
+                    );
         }
 
         #region OnModelCreating
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapearPropriedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+            foreach(var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string));
+
+                foreach(var property in properties)
+                {
+                    if (string.IsNullOrEmpty(property.GetColumnType()) && !property.GetMaxLength().HasValue)
+                    {
+                        //property.SetMaxLength(100);
+                        property.SetColumnType("VARCHAR(100)");
+                    }
+                }
+            }
         }
 
         #endregion
