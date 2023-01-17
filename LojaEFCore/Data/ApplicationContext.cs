adc69c3a6c6e89aaa09251056c2fd1c681f5658c
Caique Burssed <caique.burssed@gmail.com>
2023-01-17 11:45:43 -0300

    Adicionando registros em massa no banco de dados

--- LojaEFCore/Data/ApplicationContext.cs
+++ LojaEFCore/Data/ApplicationContext.cs
@@ -1,6 +1,7 @@
 ﻿using LojaEFCore.Data.Configurations;
 using LojaEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,13 +10,18 @@ namespace LojaEFCore.Data
 {
     class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
+
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Produto> Produtos{ get; set; }
         public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=localhost;Database=LojaEFCore;Trusted_Connection=True;");
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(@"Server=localhost;Database=LojaEFCore;Trusted_Connection=True;");
         }
 
         #region OnModelCreating
--- LojaEFCore/Program.cs
+++ LojaEFCore/Program.cs
@@ -10,16 +10,7 @@ namespace LojaEFCore
     {
         static void Main(string[] args)
         {
-            /*using var db = new Data.ApplicationContext();
-
-            var existe = db.Database.GetPendingMigrations().Any();
-
-            if (existe)
-            {
-                //MessageBox.Show("Migrations pendentes...");
-            }*/
-
-            InserirDados();
+            InserirDadosEmMassa();
         }
 
         private static void InserirDados()
@@ -38,6 +29,56 @@ namespace LojaEFCore
             //db.Set<Produto>().Add(produto);
             //db.Entry(produto).State = EntityState.Added;
             db.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total registros: {registros}");
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+            var produto = new Produto
+            {
+                Descricao = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            var cliente = new Cliente
+            {
+                Nome = "Caique Burssed",
+                CEP = "11111222",
+                Cidade = "Bragança Paulista",
+                Estado = "SP",
+                Telefone = "99000001111"
+            };
+
+            var listaClientes = new[]
+            {
+                new Cliente
+                {
+                    Nome = "Lucas Garcia",
+                CEP = "00000999",
+                Cidade = "Bragança Paulista",
+                Estado = "SP",
+                Telefone = "99000001111"
+                },
+
+                new Cliente
+                {
+                    Nome = "Fabio Ishizu",
+                CEP = "99999000",
+                Cidade = "Bragança Paulista",
+                Estado = "SP",
+                Telefone = "99000001111"
+                },
+            };
+
+            using var db = new Data.ApplicationContext();
+            //db.AddRange(produto, cliente);
+            //db.Set<Cliente>().AddRange(listaClientes);
+            db.Clientes.AddRange(listaClientes);
 
             var registros = db.SaveChanges();
             Console.WriteLine($"Total registros: {registros}");
